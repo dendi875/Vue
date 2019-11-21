ae58293f12e22784f67417b47b544c58853fb63f
quan.zhang <quan.zhang@guanaitong.com>
2019-11-21 15:02:24 +0800

    feat: 添加首页header区域开发

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App' // webpack 会找 ./App.Vue
 import router from './router' // 会默认打 index.js
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
