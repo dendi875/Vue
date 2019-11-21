feb3431381b5c6f26d2bcc11345e42d7960d8972
quan.zhang <quan.zhang@guanaitong.com>
2019-11-21 14:01:59 +0800

    feat: init project

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App' // webpack 会找 ./App.Vue
+import router from './router' // 会默认打 index.js
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
+  router, // 这是 ES6写法，等价于 route:route
+  components: { App }, // 这是 ES6 的写法，等价于 App:App，App 是一个局部组件被注册到实例中
   template: '<App/>'
 })
