e789b232a84861bc20055b5bfcfe73335f0a4514
quan.zhang <quan.zhang@guanaitong.com>
2019-11-22 14:55:03 +0800

    feat: 热门榜单区域开发

---
+++ Travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="title">本周热门榜单</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendedList"
+          :key="item.id"
+      >
+        <img class="item-img" :src="item.imgUrl"/>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data: function () {
+    return {
+      recommendedList: [
+        {
+          id: 1,
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_250x250_57ac4faa.jpg',
+          title: '上海迪士尼乐园',
+          desc: '全球最大的迪士尼城堡'
+        },
+        {
+          id: 2,
+          imgUrl: 'http://img1.qunarzz.com/tuan/team2/1507/2c/83e0e0e7ae082a.jpg_200x200_6d7378b2.jpg',
+          title: '东方明珠',
+          desc: '259米透明悬空观光廊了解一下~'
+        },
+        {
+          id: 3,
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1906/19/19b7939928a0200da3.img.jpg_200x200_6879c7f7.jpg',
+          title: '上海海昌海洋公园',
+          desc: '感受海洋公园乐趣'
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
