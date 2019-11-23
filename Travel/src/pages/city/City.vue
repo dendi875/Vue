ad880465b6927bcf7e1f5b0f6dead7daf47c2de7
quan.zhang <quan.zhang@guanaitong.com>
2019-11-23 13:52:23 +0800

    feat: 城市搜索框布局和城市列表布局

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -1,14 +1,22 @@
 <template>
+  <div>
     <city-header></city-header>
+    <city-search></city-search>
+    <city-list></city-list>
+  </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
 
 export default {
   name: 'City',
   components: {
-    CityHeader: CityHeader
+    CityHeader: CityHeader,
+    CitySearch: CitySearch,
+    CityList: CityList
   }
 }
 </script>
