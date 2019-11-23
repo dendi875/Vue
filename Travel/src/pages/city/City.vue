71ff865fd099ccd589b146ebdf46fe2d3a157b5c
quan.zhang <quan.zhang@guanaitong.com>
2019-11-23 16:25:57 +0800

    feat: 城市列表页面的动态数据渲染

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -2,12 +2,13 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :hotCities="hotCities" :cities="cities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -20,6 +21,30 @@ export default {
     CitySearch: CitySearch,
     CityList: CityList,
     CityAlphabet: CityAlphabet
+  },
+  data: function () {
+    return {
+      hotCities: [],
+      cities: {}
+    }
+  },
+  mounted: function () {
+    this.getCityInfo()
+  },
+  methods: {
+    getCityInfo: function () {
+      axios
+        .get('/api/city.json')
+        .then(this.handleGetCityInfoSuccess)
+    },
+    handleGetCityInfoSuccess: function (response) {
+      response = response.data
+      if (response.ret && response.data) {
+        const data = response.data
+        this.hotCities = data.hotCities
+        this.cities = data.cities
+      }
+    }
   }
 }
 </script>
