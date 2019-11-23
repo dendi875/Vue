fb6a7d58df3c0123177f7d97b7416bf49cc5b315
quan.zhang <quan.zhang@guanaitong.com>
2019-11-23 14:09:09 +0800

    feat: better-scroll 滚动插件的使用

--- Travel/src/pages/city/components/List.vue
+++ Travel/src/pages/city/components/List.vue
@@ -1,90 +1,96 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper"><div class="button">北京</div></div>
-        <div class="button-wrapper"><div class="button">北京</div></div>
-        <div class="button-wrapper"><div class="button">北京</div></div>
-        <div class="button-wrapper"><div class="button">北京</div></div>
-        <div class="button-wrapper"><div class="button">北京</div></div>
-        <div class="button-wrapper"><div class="button">北京</div></div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper"><div class="button">北京</div></div>
+          <div class="button-wrapper"><div class="button">北京</div></div>
+          <div class="button-wrapper"><div class="button">北京</div></div>
+          <div class="button-wrapper"><div class="button">北京</div></div>
+          <div class="button-wrapper"><div class="button">北京</div></div>
+          <div class="button-wrapper"><div class="button">北京</div></div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
+      <div class="area">
+        <div class="title border-topbottom">B</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">C</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
+      <div class="area">
+        <div class="title border-topbottom">C</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">D</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
-        <div class="item border-bottom">阿拉</div>
+      <div class="area">
+        <div class="title border-topbottom">D</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+          <div class="item border-bottom">阿拉</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted: function () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
 }
 </script>
 
