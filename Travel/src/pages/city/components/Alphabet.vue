462040c8228fbf90af2752da99b94e73e036cb97
quan.zhang <quan.zhang@guanaitong.com>
2019-11-25 11:22:53 +0800

    feat: 兄弟组件之间的联动
    拖拽城市列表页面右侧字母后左侧显示相应的内容

--- Travel/src/pages/city/components/Alphabet.vue
+++ Travel/src/pages/city/components/Alphabet.vue
@@ -2,11 +2,15 @@
   <ul class="list">
     <li
       class="item"
-      v-for="(item, key) of cities"
-      :key="key"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
       @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
     >
-      {{key}}
+      {{item}}
     </li>
   </ul>
 </template>
@@ -20,10 +24,40 @@ export default {
       required: true
     }
   },
+  computed: {
+    letters: function () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data: function () {
+    return {
+      touchStatus: false
+    }
+  },
   methods: {
     handleLetterClick: function (event) {
       // 子组件通过 $emit 向父组件触发一个事件，父组件监听这个事件，接着父组件又通过属性传值的方式向兄弟组件传值
       this.$emit('change', event.target.innerText)
+    },
+    handleTouchStart: function () {
+      this.touchStatus = true
+    },
+    handleTouchMove: function (e) {
+      if (this.touchStatus) { // 做滑动的处理
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd: function () {
+      this.touchStatus = false
     }
   }
 }
@@ -45,3 +79,8 @@ export default {
       text-align: center
       color: $bgColor
 </style>
+
+<!--
+我们希望只有在 touchstart 之后才可以触发 touchmove里的一些操作，所以需要在 data 里定义一个标志为 `touchStatus`（默认为false），
+当你的手指触摸时 this.touchStatus = true，当结束触摸时 this.touchStatus = false。
+-->
