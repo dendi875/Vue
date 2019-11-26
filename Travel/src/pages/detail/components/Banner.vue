10819076592fb043a081309f49581d09dc7979e1
quan.zhang <quan.zhang@guanaitong.com>
2019-11-26 13:33:20 +0800

    feat: 详情页面动态路由和详情页面banner布局

---
+++ Travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" />
+    <div class="banner-info">
+      <div class="banner-tittle">上海市浦东新区川沙新镇上海迪士尼度假区内</div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe63b;</span>
+        73
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-tittle
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+  </style>
--- Travel/src/router/index.js
+++ Travel/src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld' // @代表src目录
 import Home from '@/pages/home/Home.vue'
-import City from '@/pages/city/City'
+import City from '@/pages/city/City.vue'
+import Detail from '@/pages/detail/Detail.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    },
+    {
+      path: '/detail/:id', // 动态路由，前面必须是 /detail 开头，后面可以附加参数，参数的值放到 id变量里
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
