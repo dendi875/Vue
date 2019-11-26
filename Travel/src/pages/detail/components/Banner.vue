c7864901838389bcdf353105b0e5b073f8a83049
quan.zhang <quan.zhang@guanaitong.com>
2019-11-26 14:29:57 +0800

    feat: 公用图片画廊组件拆分

--- Travel/src/pages/detail/components/Banner.vue
+++ Travel/src/pages/detail/components/Banner.vue
@@ -1,19 +1,51 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" />
-    <div class="banner-info">
-      <div class="banner-tittle">上海市浦东新区川沙新镇上海迪士尼度假区内</div>
-      <div class="banner-number">
-        <span class="iconfont banner-icon">&#xe63b;</span>
-        73
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" />
+      <div class="banner-info">
+        <div class="banner-tittle">上海市浦东新区川沙新镇上海迪士尼度假区内</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe63b;</span>
+          73
+        </div>
       </div>
     </div>
+    <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallayClose"></common-gallary>
   </div>
 </template>
 
 <script>
+import CommonGallary from 'common/gallary/Gallary'
+
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  components: {
+    CommonGallary: CommonGallary
+  },
+  props: {
+    bannerImgs: {
+      type: Array,
+      default: function () {
+        return [
+          'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg',
+          'http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png'
+        ]
+      }
+    }
+  },
+  data: function () {
+    return {
+      showGallary: false
+    }
+  },
+  methods: {
+    handleBannerClick: function () {
+      this.showGallary = true
+    },
+    handleGallayClose: function () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 
